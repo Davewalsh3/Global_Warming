0f58235efce3c79b0cbc4592597fbd3a2f6c18ec
Rami Fareed Omar <Rami@Ramis-iMac.local>
2019-10-08 20:34:48 +0200

    Graphs updated

--- js/graphs.js
+++ js/graphs.js
@@ -4,21 +4,22 @@ queue()
 
 function makeGraphs(error, annualData) {
     var ndx = crossfilter(annualData);
+    // console.log(annualData);
+
     var date_dim = ndx.dimension(dc.pluck("Year"));
-    var temperature_yearly = date_dim.group().reduceSum(dc.pluck("Mean"));
+    var temperature_monthly = date_dim.group().reduceSum(dc.pluck("Mean"));
 
 
     dc.lineChart("#monthly_chart")
-        .width(1000)
+        .width(1200)
         .height(350)
         .margins({ top: 25, right: 50, bottom: 30, left: 50 })
         .dimension(date_dim)
-        .group(temperature_yearly)
+        .group(temperature_monthly)
         .transitionDuration(500)
-        .x(d3.time.scale().ordinal)
+        .x(d3.scale.ordinal())
+        .xUnits(dc.units.ordinal)
         .xAxisLabel("Year")
         .yAxis().ticks(4);
-
     dc.renderAll();
-
-};
+}
