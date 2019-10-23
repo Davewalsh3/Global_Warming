9d3e71d7aef63761d75314457ef023fa6c8b7f7d
Rami <49429174+RamiFareed1989@users.noreply.github.com>
2019-10-23 19:09:17 +0200

    changed piechart

--- js/graph4.js
+++ js/graph4.js
@@ -12,7 +12,7 @@ function doGraphs(error, cumulative_co_emissionsData) {
 
     dc.pieChart("#cumulative_carbon_emissions")
         .height(500)
-        .innerRadius(130)
+        .innerRadius(100)
         .dimension(date_dim)
         .group(cumulative_emissions)
         .transitionDuration(500)
